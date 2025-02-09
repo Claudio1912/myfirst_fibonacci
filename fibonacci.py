1262aae0a9f0f2d904065fb6e5b89af95d58d6ca
Claudio1912 <44671773+Claudio1912@users.noreply.github.com>
2025-02-09 16:26:17 +0100

    Add files via upload

---
+++ fibonacci.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: iso-8859-15 -*-
+
+numero = int(input("Quanti termini? "))
+
+# n1, n2 = 0, 1   OPPURE
+
+n1 = 0
+n2 = 1
+count = 0
+
+if numero <= 0:
+   print("Prego inserire un intero")
+elif numero == 1:
+   print(n2)
+else:
+   print("Sequenza di Fibonacci:")
+   while count < numero:
+       print(n1)
+       esimo = n1 + n2
+       n1 = n2
+       n2 = esimo
+       count += 1
